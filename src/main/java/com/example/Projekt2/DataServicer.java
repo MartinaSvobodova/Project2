ede1576c02f5e7ca7540f9c4591a881707b44e72
Martina Svobodova <xsvobod9@fi.muni.cz>
2023-08-25 17:10:24 +0200

    Start

---
+++ src/main/java/com/example/Projekt2/DataServicer.java
@@ -0,0 +1,4 @@
+package com.example.Projekt2;
+
+public class DataServicer {
+}
---
+++ src/main/java/com/example/Projekt2/MainController.java
@@ -0,0 +1,19 @@
+package com.example.Projekt2;
+
+import org.springframework.web.bind.annotation.GetMapping;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+
+public class MainController {
+
+    @GetMapping("/hello")
+
+    public String hello() {
+
+        return "Hello world!";
+
+    }
+
+}
